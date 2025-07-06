c99340eb352b2036164399227b0c868943fed79f
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-07-06 19:46:05 +0900

    セキュリティグループの追加

---
+++ modules/sg/variables.tf
@@ -0,0 +1,38 @@
+variable "environment" {
+  type        = string
+  description = "環境名 (stg or prd)"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  description = "インバウンドルール"
+}
+
+variable "egress_rules" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  description = "アウトバウンドルール"
+  default = [{
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
+}
