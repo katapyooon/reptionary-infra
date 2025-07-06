be525c03c2f45edd3ddcf686ccfd584908ac0301
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-07-06 16:50:56 +0900

    subnetとigwを追加

--- modules/vpc/valuables.tf
+++ modules/vpc/valuables.tf
@@ -2,12 +2,23 @@ variable "environment" {
   type        = string
 }
 
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+  default     = null
+}
+
 variable "vpc_cidr" {
   type        = string
   description = "VPCのCIDRブロック"
 }
 
 variable "public_subnet_cidrs" {
+  type        = map(string)
+  description = "パブリックサブネットのCIDRブロック"
+}
+
+variable "private_subnet_cidrs" {
   type        = map(string)
   description = "パブリックサブネットのCIDRブロック"
 }
