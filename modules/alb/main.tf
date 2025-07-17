84c1a8f9dcbf3d60174adda4b60a88ef20749f7f
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-07-17 23:27:43 +0900

    ターゲットグループの追加

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -12,3 +12,35 @@ resource "aws_alb" "alb" {
     ManagedBy   = "terraform"
   }
 }
+
+# ================
+# Target Group
+# ================
+resource "aws_lb_target_group" "alb" {
+  name                 = "reptionary-${var.environment}-tg"
+  vpc_id               = var.vpc_id
+  port                 = 80
+  target_type          = "ip"
+  protocol             = "HTTP"
+  deregistration_delay = 300
+  health_check {
+    path                = "/api/health_checks"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+}
